eae291b926867d95e9f9704cb208cbf8e5007b53
tungnb2001 <lethanhtung312001@gmail.com>
2023-10-22 20:00:24 +0700

    search by name or email

--- app/src/main/java/com/example/pe_prm392/MainActivity.java
+++ app/src/main/java/com/example/pe_prm392/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.pe_prm392.Adapter.UserAdapter;
 import com.example.pe_prm392.Models.User;
@@ -23,14 +26,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText searchByNameandEmail;
+    private UserAdapter userAdapter = new UserAdapter(new ArrayList<>());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchByNameandEmail = findViewById(R.id.editTextSearch);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        UserAdapter userAdapter = new UserAdapter(new ArrayList<>());
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(userAdapter);
 
@@ -61,6 +67,54 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("API Call", "Error: " + t.getMessage());
             }
         });
+
+        searchByNameandEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String searchText = s.toString();
+                performSearch(searchText, apiService);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+    private void performSearch(String searchText, ApiService apiService) {
+        Call<User.SearchResponse> call = apiService.searchUsers(searchText, searchText);
+        call.enqueue(new Callback<User.SearchResponse>() {
+            @Override
+            public void onResponse(Call<User.SearchResponse> call, Response<User.SearchResponse> response) {
+                if (response.isSuccessful()) {
+                    List<User> userList = response.body().getData();
+                    List<User> filteredUserList = new ArrayList<>();
+
+                    for (User user : userList) {
+                        if (user.getFirst_name().toLowerCase().contains(searchText.toLowerCase()) ||user.getLast_name().toLowerCase().contains(searchText.toLowerCase())   || user.getEmail().toLowerCase().contains(searchText.toLowerCase())) {
+                            filteredUserList.add(user);
+                        }
+                    }
+                    userAdapter.setUserList(filteredUserList);
+                    userAdapter.notifyDataSetChanged();
+                } else {
+                    Log.e("API Call", "Error: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User.SearchResponse> call, Throwable t) {
+                Log.e("API Call", "Error: " + t.getMessage());
+            }
+        });
     }
 
 }
+
+
+
